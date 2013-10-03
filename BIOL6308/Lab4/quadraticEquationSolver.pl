339587bc672f327f7c0ae4c33330b824c3647d89
Dan Shea <daniel.john.shea@gmail.com>
2013-10-03 18:28:14 -0400

    20131003: Lab4 solutions added

---
+++ BIOL6308/Lab4/quadraticEquationSolver.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+################################################################################
+# BIOL6308 - Bioinformatics Computational Methods I
+# Author: Dan Shea
+# Date: 2013-09-25
+# Description:
+# This program (name the program quadraticEquationSolver.pl) sould solve a
+# quadratic equation of the form:
+#
+# ax**2+bx+c
+#
+# The program asks for three coefficients in order. Quadratic coefficient cannot
+# be zero (a != 0.) The key to solve this equation is to calculate delta:
+#
+# delta = b**2 - 4ac
+#
+# Based on the sign of delta, three cases are possible:
+#
+# If delta is greater than zero then it has two separate roots:
+#
+# x1,x2 = (-b +/- sqrt(delta)) / 2a
+#
+# If delta is zero then it has a repeated root.
+#
+# If delta is less than zero, it doesn’t have real roots, and you have to show
+# an appropriate message.
+#
+################################################################################
+
+sub calculate_delta {
+    my ($a, $b, $c) = @_;
+    return($b**2 - 4*$a*$c);
+}
+
+sub solver {
+    my ($coefficient0, $coefficient1, $delta) = @_;
+    my $root1 = (-$coefficient1+sqrt($delta))/(2*$coefficient0);
+    my $root2 = (-$coefficient1-sqrt($delta))/(2*$coefficient0);
+    return($root1,$root2);
+}
+
+my @coefficients;
+for my $prompt (qw(a b c)){
+    print "Please enter the $prompt coefficient: ";
+    my $input = <STDIN>;
+    chomp $input;
+    push(@coefficients, $input);
+}
+
+# Did the user give us a 0 as the a coefficient?
+if ($coefficients[0] == 0) {
+    print "The quadratic coefficient cannot be zero.\n";
+    exit(1);
+}
+
+my $delta = &calculate_delta(@coefficients);
+print "The delta is $delta\n";
+if ($delta < 0) {
+    print "The roots of this equation are imaginary.\n";
+}
+elsif ($delta == 0) {
+    printf("The root is %f\n", -$coefficients[1]/(2*$coefficients[0]));
+}
+else {
+    my ($root1, $root2) = &solver(@coefficients, $delta);
+    printf("The first root is %f\n", $root1);
+    printf("The second root is %f\n", $root2);
+}
+exit(0);
